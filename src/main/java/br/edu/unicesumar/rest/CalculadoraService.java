2e8db6f2fc4a9f19c99eec456cbdd3685ad8bb09
Marcello Endo <marcelloendo@hotmail.com>
2019-04-27 10:48:25 -0300

    deploy1

---
+++ src/main/java/br/edu/unicesumar/rest/CalculadoraService.java
@@ -0,0 +1,22 @@
+package br.edu.unicesumar.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/calculadora")
+public class CalculadoraService {
+
+	@GET
+	@Path("/soma/{param1}/{param2}")
+	public Response somar(@PathParam("param1") Integer parametro1, @PathParam("param2") Integer parametro2) {
+
+		Integer soma = parametro1 + parametro2;
+		String result = "A soma é " + soma;
+
+		return Response.status(200).entity(result).build();
+
+	}
+
+}
