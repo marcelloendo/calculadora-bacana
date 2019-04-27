153dcb0f47673a712ba9a580e2f540624d6bc011
Marcello Endo <marcelloendo@hotmail.com>
2019-04-27 12:00:55 -0300

    dpl

--- src/main/java/br/edu/unicesumar/model/Produto.java
+++ src/main/java/br/edu/unicesumar/model/Produto.java
@@ -1,8 +1,22 @@
 package br.edu.unicesumar.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column
 	private String nome;
+	
+	@Column
 	private int quantidade;
 
 	public String getNome() {
@@ -25,5 +39,6 @@ public class Produto {
 	public String toString() {
 		return "Produto [nome=" + nome + ", quantidade=" + quantidade + "]";
 	}
+
 	
 }
